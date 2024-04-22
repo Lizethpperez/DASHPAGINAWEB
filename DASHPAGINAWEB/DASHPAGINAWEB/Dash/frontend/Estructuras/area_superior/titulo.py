fcaf1f7c4b65b68b8ed593532fcb2dc6d0b3afd9
Lizethpperez <162075482+Lizethpperez@users.noreply.github.com>
2024-04-22 03:56:00 -0500

    Delete DASHPAGINAWEB directory

--- DASHPAGINAWEB/DASHPAGINAWEB/Dash/frontend/Estructuras/area_superior/titulo.py
+++
@@ -1,9 +0,0 @@
-from dash import html
-import dash_bootstrap_components as dbc
-
-variableA = dbc.Container([
-    dbc.Row([
-        dbc.Col('LOGO', md=14, style={'background-color': 'red'}),
-        dbc.Col('DISEÑA TU CANAL', md=14, style={'background-color': 'skyblue'}, align="center"),
-    ])
-])
